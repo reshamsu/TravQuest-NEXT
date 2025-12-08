43ec14c864eb5d11d9666dc7fe6a583c2d8319d7
reshamsu <resham.iox@gmail.com>
2025-12-08 13:12:50 +0530

    TravQuest Login Build

--- app/GoogleProvider.tsx
+++ app/GoogleProvider.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import type { ReactNode } from "react";
 
-export default function GoogleProvider({ children }) {
+export default function GoogleProvider({ children }: { children: ReactNode }) {
   return (
     <GoogleOAuthProvider
-      clientId={
-        "454373247918-uk8j5em60iu73367qho1vdfpaddma36c.apps.googleusercontent.com"
-      }
+      clientId="454373247918-uk8j5em60iu73367qho1vdfpaddma36c.apps.googleusercontent.com"
     >
       {children}
     </GoogleOAuthProvider>
