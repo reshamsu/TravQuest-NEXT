45d579e95141d9d9c4d5a98dcad0163857d21d24
reshamsu <resham.iox@gmail.com>
2025-12-13 09:56:16 +0530

    Build Check-in

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,8 @@
 import Hero from "@/components/contact/Hero";
 import Info from "@/components/contact/Info";
 
+export const dynamic = "force-dynamic";
+
 export default function Contact() {
   return (
     <div className="relative z-30">
